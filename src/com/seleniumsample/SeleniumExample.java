fcd9a3a4eba72c75ec746adfd753ae8c82e30f61
sureshkumar0688 <kumar12201997@gmail.com>
2022-08-28 23:08:08 +0530

    selenium project

---
+++ src/com/seleniumsample/SeleniumExample.java
@@ -0,0 +1,14 @@
+package com.seleniumsample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumExample {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","D:\\Java Software\\SeleniumJava\\Driver\\chromedriver.exe" );
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/login/");
+		
+	}
+
+}
